40146734a31c531f38e3b2df080b408aff183916
Jhonatan Antonio Hernandez Sarmiento <john_daviz_hdz@hotmail.com>
2022-04-22 07:48:45 -0500

    test actualizado y funcionando

--- test/missionCommander.test.js
+++ test/missionCommander.test.js
@@ -1,7 +1,8 @@
+const MissionCommander = require('./../app/missionCommander');
+
 describe("Esto es una suite de pruebas", () => {
-    test('Caso de prueba 1', () => {
-      const result = 1 + 2 
-      expect(result).toBe(10);
-    });
-  })
-  
+  test('Caso de prueba 1', () => {
+    const myMissionCommander = new MissionCommander("Woopa")
+    expect(myMissionCommander.name).toBe("Woopa");
+  });
+})
